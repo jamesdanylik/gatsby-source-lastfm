145819fe83d82a7590e527f4bf399ab9bf995790
james <james.danylik@gmail.com>
2018-04-06 13:34:20 -0700

    First working version; limited to most recent scrobbles

--- README.md
+++ README.md
@@ -1,19 +1,154 @@
 # gatsby-source-lastfm
 
-This is a source plugin for GatsbyJS to pull information from Last.FM.  Currently, it is *not ready for use*; in fact, right now it ***DOESN'T WORK AT ALL***. this is my first gatsby source plugin and there is no real documentation on how to write a source plugin, so expect development to proceed accordingly.
+This is a source plugin for GatsbyJS to pull information from Last.FM.  Currently, the plugin grabs only the most recent 200 scrobbles for a single Last.fm user. Grabbing more / all scrobbles for a user is currently a work-in-progress; other features added by request.
 
-## Usage ***NOT WORKING YET***
 
-### gatsby-config.js
+## Install
+
+```bash
+npm install --save gatsby-source-lastfm
+```
+
+
+## Configuration
 ```javascript
+// In your gatsby-config.js
 plugins: [
 	{
 	      resolve: "gatsby-source-lastfm",
 	      options: {
-	        apiKey: '<<YOUR API KEY HERE>>',
+	        api_key: '<<YOUR API KEY HERE>>',
 	        username: '<<USERNAME TO TRACK>>'
 	      },
 	},
 	...
 ]
+```
+
+## Provided Queries
+
+### Playbacks
+```graphql
+  allLastfmPlayback {
+    edges {
+      node {
+        id
+  		date {
+  		  uts
+  		  text
+  		}
+        track {
+          id
+          # linked track node here
+          # (See track query for all fields)
+        }
+      }
+    }
+  }
+```
+### Tracks
+```graphql
+  allLastfmTrack {
+    edges {
+      node {
+        id
+        name
+        loved
+        mbid
+        streamable
+        url
+        image {
+          text
+          size
+        }
+        artist {
+          id
+          # linked artist node here!
+          # (See artist query for all fields)
+        }
+        album {
+          id
+          # linked album node here!
+          # (See album query for all fields)
+        }
+        playbacks {
+          id
+          # linked playback nodes here!
+          # (See playbacks query for all fields)
+        }
+      }
+    }
+  }
+```
+
+### Albums
+```graphql
+  allLastfmAlbum {
+    edges {
+      node {
+        id
+        name
+        mbid
+        playbacks {
+          id
+          # linked playback nodes here!
+          # (See playbacks query for all fields)
+        }
+        artist {
+          id
+          # linked artist node here!
+          # (See artist query for all fields)
+        }
+        tracks {
+          id
+          # linked track nodes here
+          # (See track query for all fields)
+        }
+      }
+    }
+  }
+```
+
+### Artists
+```graphql
+  allLastfmArtist {
+    edges {
+      node {
+        id
+        name
+        mbid
+        image {
+          text
+          size
+        }
+        playbacks {
+          id
+          # linked playback nodes here!
+          # (See playbacks query for all fields)
+        }
+        albums {
+          id
+          # linked album nodes here!
+          # (See album query for all fields)
+        }
+        tracks {
+          id
+          # linked track nodes here
+          # (See track query for all fields)
+        }
+      }
+    }
+  }
+```
+
+### Meta
+```graphql
+  allLastfmMeta {
+    edges {
+      node {
+        id
+        total_playbacks
+      }
+    }
+  }
 ```
--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,18 +1,158 @@
-import {RecentTrackNode} from './nodes'
+import {
+	PlaybackNode, 
+	TrackNode, 
+	ArtistNode,
+	AlbumNode,
+	MetaNode,
+	generateNodeId, 
+	TRACK_TYPE, 
+	PLAYBACK_TYPE,
+	ARTIST_TYPE,
+	ALBUM_TYPE,
+	META_TYPE
+} from './nodes'
+import fetch from 'isomorphic-fetch'
 
-exports.sourceNodes = async({boundActionCreators}) => {
+function api(url) {
+	return fetch(url).then(results=> {
+		return results.json()
+	})
+}
+
+function recentTracksUrl(options) {
+	var url = 'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&format=json'
+	for( var property in options ) {
+		url += '&' + property + '=' + options[property]
+	}
+	return url
+}
+
+function transformText(orig) {
+	var trans = []
+	for(var index in orig) {
+		var item = {}
+		for(var key in orig[index]) {
+			if(key === "#text") {
+				item['text'] = orig[index][key]
+			} else {
+				item[key] = orig[index][key]
+			}
+		}
+		trans.push(item)
+	}
+	return trans
+}
+
+exports.sourceNodes = async({boundActionCreators}, {
+	api_key,
+	username
+}) => {
 	const { createNode } = boundActionCreators
 
-	const recentTracks = [
-		{id: 0, test: "stuff"},
-		{id:1, test: "things"},
-		{id:2, test: "also stuff"},
-	]
+	console.log('\nStarting LastFM Scrape...')
+	console.log('  api_key=' + api_key)
+	console.log('  username=' + username)
 
-	recentTracks.forEach(track => {
-		const recentTrackNode = RecentTrackNode(track)
-		createNode(recentTrackNode)
+	const response = await api(recentTracksUrl({api_key: api_key, username: username, extended: 1, limit: 200}))
+
+	var tracks = {}
+	var artists = {}
+	var albums = {}
+	var playbacks = []
+
+	response.recenttracks.track.forEach(track => {
+		const albumUrl = track.artist.url + '/' + encodeURIComponent(track.album['#text'].replace(/ /g, '+'))
+		// CHECK TRACK NODE
+		if( tracks[generateNodeId(TRACK_TYPE, track.url)] ) {
+			tracks[generateNodeId(TRACK_TYPE, track.url)].playbacks___NODE.push(generateNodeId(PLAYBACK_TYPE, track.date.uts))
+		} else {
+			var trackNode = TrackNode({
+				id: track.url,
+				name: track.name,
+				loved: track.loved,
+				mbid: track.mbid,
+				streamable: track.streamable,
+				url: track.url,
+				image: transformText(track.image),
+				playbacks___NODE: [generateNodeId(PLAYBACK_TYPE, track.date.uts)],
+				artist___NODE: generateNodeId(ARTIST_TYPE, track.artist.url),
+				album___NODE: generateNodeId(ALBUM_TYPE, albumUrl),
+			})
+			tracks[generateNodeId(TRACK_TYPE, track.url)] = trackNode
+		}
+
+		// CHECK ARTIST NODE
+		if( artists[generateNodeId(ARTIST_TYPE, track.artist.url)]) {
+			artists[generateNodeId(ARTIST_TYPE, track.artist.url)].playbacks___NODE.push(generateNodeId(PLAYBACK_TYPE, track.date.uts))
+			// If track not linked to artist yet, link
+			if(artists[generateNodeId(ARTIST_TYPE, track.artist.url)].tracks___NODE.indexOf(generateNodeId(TRACK_TYPE, track.url)) < 0) {
+				artists[generateNodeId(ARTIST_TYPE, track.artist.url)].tracks___NODE.push(generateNodeId(TRACK_TYPE, track.url))
+			}
+		} else {
+			var artistNode = ArtistNode({
+				id: track.artist.url,
+				name: track.artist.name,
+				mbid: track.artist.mbid,
+				image: transformText(track.artist.image),
+				playbacks___NODE: [generateNodeId(PLAYBACK_TYPE, track.date.uts)],
+				albums___NODE: [generateNodeId(ALBUM_TYPE, albumUrl)],
+				tracks___NODE: [generateNodeId(TRACK_TYPE, track.url)]
+			})
+			artists[generateNodeId(ARTIST_TYPE, track.artist.url)] = artistNode
+		}
+
+		// CHECK ALBUM NODE
+		if( albums[generateNodeId(ALBUM_TYPE, albumUrl)] ) {
+			albums[generateNodeId(ALBUM_TYPE, albumUrl)].playbacks___NODE.push(generateNodeId(PLAYBACK_TYPE, track.date.uts))
+			// if track not linked to album yet, link
+			if(albums[generateNodeId(ALBUM_TYPE, albumUrl)].tracks___NODE.indexOf(generateNodeId(TRACK_TYPE, track.url)) < 0) {
+				albums[generateNodeId(ALBUM_TYPE, albumUrl)].tracks___NODE.push(generateNodeId(TRACK_TYPE, track.url))
+			}
+		} else {
+			var albumNode = AlbumNode({
+				id: albumUrl,
+				name: track.album['#text'],
+				mbid: track.album.mbid,
+				playbacks___NODE: [generateNodeId(PLAYBACK_TYPE, track.date.uts)],
+				artist___NODE: generateNodeId(ARTIST_TYPE, track.artist.url),
+				tracks___NODE: [generateNodeId(TRACK_TYPE, track.url)]
+			})
+			albums[generateNodeId(ALBUM_TYPE, albumUrl)] = albumNode
+		}
+
+		const playbackNode = PlaybackNode({
+			id: track.date.uts, 
+			date: transformText([track.date])[0],
+			track___NODE: generateNodeId(TRACK_TYPE, track.url)
+		})
+		playbacks.push(playbackNode)
 	})
+
+	console.log(Object.keys(tracks).length + " Tracks")
+	for(var key in tracks) {
+		createNode(tracks[key])
+	}
+
+	console.log(Object.keys(albums).length + " Albums")
+	for(var key in albums) {
+		createNode(albums[key])
+	}
+
+	console.log(Object.keys(artists).length + " Artists")
+	for(var key in artists) {
+		createNode(artists[key])
+	}
+
+	console.log(playbacks.length + " Playbacks")
+	playbacks.forEach(playback => {
+		createNode(playback)
+	})
+
+	const metaNode = MetaNode({
+		id: 0,
+		total_playbacks: response.recenttracks['@attr'].total
+	})
+	createNode(metaNode)
 
 	return
 }
