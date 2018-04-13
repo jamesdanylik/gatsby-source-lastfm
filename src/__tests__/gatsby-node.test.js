a0c1563d2852dc258ef1f55ec452e568a9cec30b
james <james.danylik@gmail.com>
2018-04-13 08:35:49 -0700

    Test basic API response

--- src/__tests__/gatsby-node.test.js
+++ src/__tests__/gatsby-node.test.js
@@ -6,6 +6,20 @@ test('always pass', () => {
 	expect(1).toBe(1)
 })
 
+//TESTAPIKEY
+//TESTUSERNAME
+
 test('sercure environment vairables', () => {
 	expect(process.env.TESTVAR).toBe("TESTVAR")
 })
+
+test('basic API response test', () => {
+	const response = await api(recentTracksUrl({
+		api_key: process.env.TESTAPIKEY, 
+		username: process.env.TESTUSERNAME, 
+		extended: 1, 
+		limit: 200
+	}))
+	expect(response.hasOwnProperty('recenttracks')).toBe(true)
+	expect(response.recenttracks.hasOwnProperty('track')).toBe(true)
+}
